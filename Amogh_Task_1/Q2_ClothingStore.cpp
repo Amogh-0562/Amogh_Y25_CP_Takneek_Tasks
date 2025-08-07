c4305c7776b66c8c94d52b9550fa5e110f8da651
Amogh-0562 <amoghgrind@gmail.com>
2025-08-07 12:38:44 +0530

    Create Q2_ClothingStore.cpp

---
+++ Amogh_Task_1/Q2_ClothingStore.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int x,y,z,a,b,c,t;
+	std::cin >> t;
+	for (int i = 0; i < t; i++){
+	    int count = 0;
+	    std::cin >> x >> y >> z >> a >> b >> c;
+	    if (z>c){
+	        y += z-c;
+	        z = c;
+	        
+	        count += c;
+	    }
+	    else{
+	        count += z;
+	    }
+	    if (y>b){
+	        x += y-b;
+	        y = b;
+	        
+	        count += b;
+	    }
+	    else{
+	        count += y;
+	    }
+	    if (x>a){
+	        count += a;
+	    }
+	    else{
+	        count += x;
+	    }
+	    
+	    std:cout << count << '\n';
+	}
+	
+
+}
