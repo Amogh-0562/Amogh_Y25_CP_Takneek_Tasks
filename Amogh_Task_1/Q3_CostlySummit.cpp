ac5e60cbe5c0020fe2689ad933d1002a166d65d8
Amogh-0562 <amoghgrind@gmail.com>
2025-08-08 14:29:56 +0530

    Update Q3_CostlySummit.cpp

--- Amogh_Task_1/Q3_CostlySummit.cpp
+++ Amogh_Task_1/Q3_CostlySummit.cpp
@@ -18,6 +18,8 @@ int main() {
 	        sum += count;
 	    }
 	    std::unordered_map<std::string, int> dict;
+
+		//here i am making a mapping where the key is the language and the value is the number of people speaking it
 	    for (int j = 0; j < n; j++){
 	        if (dict.contains(std::string(1,s[j]))){
 	            dict[std::string(1,s[j])] += 1; 
@@ -26,14 +28,24 @@ int main() {
 	            dict[std::string(1,s[j])] = 1;
 	        }
 	    }
+
+		//this is a vector contaning only the number of people speaking a language
 	    std::vector<int> nums;
         for (const auto& pair : dict) {
             nums.push_back(pair.second);
         }
 
-    
+    	//sorting it
         std::sort(nums.begin(), nums.end());
-        
+
+		// here comes the main part of the logic. we have to keep using the translator until learning the language is cheaper.
+		// if we make more popular languages use the translator first, that would be non optimal as we would have to learn the smaller languages
+		// so if we do reach a point where learning is cheaper, we ought to learn the bigger languages
+		// hence i sorted the vector having the number of people speaking each language
+		// now i start with smallest languages, using translator by default. i check after translating my way through each language if learning it would have been cheaper
+		// if it would have been, then we dont use the translator and learn it. and since the vector is in ascending order, all languages after it also would be cheaper to learn.
+		
+		
         int counter = 0;
         int current_count = 0;
         int flag = 1;
